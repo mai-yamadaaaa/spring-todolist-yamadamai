38b2ce5a114c28ef3f1cd343045c5f8c35ce80b7
山田麻衣 <yamad425@gmail.com>
2025-04-01 14:59:08 +0900

    repository

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repository;
 
-public class TaskRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+	List<Task> findByUserId(Integer userId);
 
 }
