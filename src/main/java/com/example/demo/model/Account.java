679f11a714b8e2c3f886858de891238777c41bb1
山田麻衣 <yamad425@gmail.com>
2025-04-01 14:59:34 +0900

    model

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,5 +1,43 @@
 package com.example.demo.model;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
 public class Account {
+
+	//フィールド
+	private String name;
+
+	private String email;
+
+	private String password;
+
+	//ゲットメソッド
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	//セットメソッド
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
